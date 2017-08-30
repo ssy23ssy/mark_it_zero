a388200a90ef17783e938ae22786b0ce8d9f6266
ssy23ssy <ssy23ssy@naver.com>
2017-08-30 18:22:16 +0900

    Update mark_it_zero.js

--- app/assets/javascripts/mark_it_zero/mark_it_zero.js
+++ app/assets/javascripts/mark_it_zero/mark_it_zero.js
@@ -1,4 +1,4 @@
-$(document).on(turbolink:load, function(){
+$(document).on(turbolinks:load, function(){
   $('textarea.mark-it-zero').each(function(idx){
     new Editor({
       element: $('textarea.mark-it-zero')[idx]
